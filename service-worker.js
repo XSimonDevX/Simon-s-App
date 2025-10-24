0b53cbe437d88018c831c46fe06595478f224e0f
XSimonDevX <cvfgty111@gmail.com>
2025-10-24 16:35:54 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 // Bump this any time you want clients to fetch a fresh bundle
-const CACHE_NAME = "flashcards-v13-idb";
+const CACHE_NAME = "flashcards-v8-idb";
 
 const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
   // bump the query version whenever script.js changes
-  "./script.js?v13",
+  "./script.js?v=8",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png",
@@ -35,10 +35,6 @@ const ASSETS = [
   "./img/mom.png",
   "./img/dad.png",
   "./img/grandma.png"
-
-  "./img/noddy.png",
-  "./img/peppa.png",
-  
 ];
 
 self.addEventListener("install", (event) => {
