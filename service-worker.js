2ddc8ff87395be0fe672772bf6c9d64e7494188e
XSimonDevX <cvfgty111@gmail.com>
2025-10-25 19:56:51 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 // Bump this any time you want clients to fetch a fresh bundle
-const CACHE_NAME = "flashcards-v36-idb";
+const CACHE_NAME = "flashcards-v37-idb";
 
 const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
   // bump the query version whenever script.js changes
-  "./script.js?v=36",
+  "./script.js?v=37",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png",
